38262e7fb0cc8d381466f37d55b8b108d0c5cdb0
KKrokhaleva <kkrohaleva93@gmail.com>
2022-12-16 16:03:58 +0200

    added form and binding date in organizer

--- src/app/organizer/organizer.component.ts
+++ src/app/organizer/organizer.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataService} from "../shared/data.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-organizer',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./organizer.component.scss']
 })
 export class OrganizerComponent implements OnInit {
+  form: FormGroup
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(public dateService: DataService) {
   }
 
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      tittle: new FormControl('', Validators.required)
+    })
+  }
+
+  submit() {
+    const {tittle} = this.form.value
+    console.log(tittle);
+  }
 }
